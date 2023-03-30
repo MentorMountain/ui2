9379f290997804ad1c249b7e65548d677c710c72
Jarod <jarodf@sfu.ca>
2023-03-30 15:58:30 -0700

    blog: add TODOs, bypass login

--- src/login/RequireLogin.tsx
+++ src/login/RequireLogin.tsx
@@ -4,7 +4,8 @@ import { LOGIN_PAGE } from "../paths";
 import { useLoginContext } from "./auth/LoginContextProvider";
 
 export function RequireLogin({ children }: { children: JSX.Element }) {
-  const { computingID, isInitialized } = useLoginContext();
+  // const { computingID, isInitialized } = useLoginContext();
+  const { computingID, isInitialized } = {"computingID":"jarodf", "isInitialized":true}; // TODO-JAROD: remove 
   const location = useLocation();
 
   if (!isInitialized) {
