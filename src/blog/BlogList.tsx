3783fffa9ee939c175da1138ec42b3a7650c65f6
Jarod <jarodf@sfu.ca>
2023-04-02 11:36:47 -0700

    blog: Add text for when 0 blogs exist

--- src/blog/BlogList.tsx
+++ src/blog/BlogList.tsx
@@ -1,6 +1,7 @@
 import { BlogPostProps } from "./BlogPost.model";
 import BlogPost from "./BlogPost";
 import { Spinner } from "react-bootstrap";
+import { useLoginContext } from "../login/auth/LoginContextProvider";
 
 interface BlogListProps {
   isGettingBlogs: boolean;
@@ -12,6 +13,9 @@ export default function BlogList({
   blogList,
 }: BlogListProps) {
   const shouldShowSpinner: boolean = isGettingBlogs;
+  const listLoadedEmpty: boolean = !isGettingBlogs && blogList.length === 0;
+
+  const { role } = useLoginContext();
 
   return (
     <div className="blog-list">
@@ -26,6 +30,15 @@ export default function BlogList({
           aria-hidden="true"
         />
       </div>
+      <hr className={listLoadedEmpty ? "" : "visually-hidden"}></hr>
+      <p className={(listLoadedEmpty && role === "student") ? "empty-list-text" : "visually-hidden"}>
+        Whoops! Seems like there's no posts at the moment!<br/>
+        Try again a bit later after our mentors have had a chance to add some knowledge! 🙂
+      </p>
+      <p className={(listLoadedEmpty && role === "mentor") ? "empty-list-text" : "visually-hidden"}>
+        Hey Mentors! Any blog posts you create will show up down here!<br/>
+        Get started helping students by hitting the button above to contribute some experiences! 👍
+      </p>
       {
         blogList.map((blogPost) => (
           <BlogPost
