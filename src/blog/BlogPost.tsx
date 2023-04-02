42e4bd97c32916b78b7e93e0845634ffcf06968d
Jarod <jarodf@sfu.ca>
2023-04-02 13:52:50 -0700

    blog: Polish up blog post info header styling

--- src/blog/BlogPost.tsx
+++ src/blog/BlogPost.tsx
@@ -1,19 +1,5 @@
-import Image from 'react-bootstrap/Image'
-
 import { BlogPostProps } from "./BlogPost.model";
-import profilePicture from './placeholderProfilePicture.png';
-
-// Source: https://stackoverflow.com/questions/18679576/counting-words-in-string
-// Source: https://speechify.com/blog/reading-time/
-function calcTextReadTimeInMinutes(text: string): number {
-  if (text) {
-    const numOfWords = text.trim().split(/\s+/).length;
-    return Math.max(1, Math.round(numOfWords / 200));
-  } else {
-    console.error("Blog post text is undefined");
-    return 0;
-  }
-}
+import BlogPostInfoHeader from './BlogPostInfoHeader';
 
 export default function BlogPost({
     postID,
@@ -25,22 +11,10 @@ export default function BlogPost({
   return (
     <article>
       <hr></hr>
-      <header>
-        <Image src={profilePicture}
-               roundedCircle={true}
-               width="50px"
-               height="50px"/>
-        <div>
-          <p className="author-name">{authorID}</p>
-          <p className="post-info">
-            {new Date(date).toLocaleDateString("en-CA")} <span className="bullet-point">•</span>&nbsp;
-            {new Date(date).toLocaleTimeString("en-CA")} <span className="bullet-point">•</span>&nbsp;
-            {calcTextReadTimeInMinutes(content)} min read <span className="bullet-point">•</span>&nbsp;
-            {postID ? postID : "NEW POST! (Badge)"}
-          </p>
-        </div>
-      </header>
-      
+      <BlogPostInfoHeader postID={postID}
+                          authorID={authorID}
+                          date={date}
+                          content={content} />
       <section>
         <h2>{title}</h2>
         <p>{content}</p>
