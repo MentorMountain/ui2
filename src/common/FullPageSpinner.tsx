888774fbf490d4dd79cbc798bd90ba9213f9a9ed
Kyle Saburao <ksaburao@sfu.ca>
2023-04-02 15:11:19 -0700

    fix loading spinner

--- src/common/FullPageSpinner.tsx
+++ src/common/FullPageSpinner.tsx
@@ -1,5 +1,12 @@
 import { Spinner } from "react-bootstrap";
 
 export default function FullPageSpinner() {
-  return <Spinner style={{ margin: "2em" }} />;
+  return (
+    <div className="d-flex justify-content-around mt-4 pt-4">
+      <div className="d-flex flex-column text-center">
+        <Spinner style={{ width: "5rem", height: "5rem", margin: "1em" }} />
+        <p>Loading</p>
+      </div>
+    </div>
+  );
 }
