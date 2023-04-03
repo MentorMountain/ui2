ecb380a2ef12341900b90fc26a3b2c0e5bf9c1d7
Kyle Saburao <ksaburao@sfu.ca>
2023-04-03 11:27:07 -0700

    lock navbar during init

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -9,7 +9,7 @@ import { BLOG_PAGE } from "./paths";
 
 export default function NavBar() {
   const navigate = useNavigate();
-  const { username, logout } = useLoginContext();
+  const { username, logout, isInitialized } = useLoginContext();
 
   const isLoggedIn = username !== "";
 
@@ -19,39 +19,49 @@ export default function NavBar() {
         <Navbar.Brand onClick={() => navigate(HOME_PAGE)}>
           Mentor Mountain
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            {isLoggedIn && (
-              <>
-                <Nav.Link onClick={() => navigate(QUESTIONS_PAGE)}>
-                  Questions
-                </Nav.Link>
-                <Nav.Link onClick={() => navigate(BLOG_PAGE)}>Blog</Nav.Link>
-              </>
-            )}
-          </Nav>
-          <Nav>
-            {isLoggedIn && (
-              <>
-                <Nav.Link onClick={() => navigate(ABOUT_PAGE)}>About</Nav.Link>
-                <Nav.Link onClick={() => navigate(ACCOUNT_PAGE)}>
-                  Account
-                </Nav.Link>
-              </>
-            )}
-            {isLoggedIn ? (
-              <Nav.Link
-                onClick={() => logout(() => console.log("Logging out"))}
-              >
-                Logout
-              </Nav.Link>
-            ) : (
-              <Nav.Link onClick={() => navigate(LOGIN_PAGE)}>Login</Nav.Link>
-            )}
-            {isLoggedIn && <Navbar.Text>({username})</Navbar.Text>}
-          </Nav>
-        </Navbar.Collapse>
+        {isInitialized && (
+          <>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                {isLoggedIn && (
+                  <>
+                    <Nav.Link onClick={() => navigate(QUESTIONS_PAGE)}>
+                      Questions
+                    </Nav.Link>
+                    <Nav.Link onClick={() => navigate(BLOG_PAGE)}>
+                      Blog
+                    </Nav.Link>
+                  </>
+                )}
+              </Nav>
+              <Nav>
+                {isLoggedIn && (
+                  <>
+                    <Nav.Link onClick={() => navigate(ABOUT_PAGE)}>
+                      About
+                    </Nav.Link>
+                    <Nav.Link onClick={() => navigate(ACCOUNT_PAGE)}>
+                      Account
+                    </Nav.Link>
+                  </>
+                )}
+                {isLoggedIn ? (
+                  <Nav.Link
+                    onClick={() => logout(() => console.log("Logging out"))}
+                  >
+                    Logout
+                  </Nav.Link>
+                ) : (
+                  <Nav.Link onClick={() => navigate(LOGIN_PAGE)}>
+                    Login
+                  </Nav.Link>
+                )}
+                {isLoggedIn && <Navbar.Text>({username})</Navbar.Text>}
+              </Nav>
+            </Navbar.Collapse>
+          </>
+        )}
       </Container>
     </Navbar>
   );
